ee5fe60a5bec83918532abd267be6ed7ba133e41
Martin BOUCAULT <82270680+Martinouaiouaiouai@users.noreply.github.com>
2023-12-24 23:42:41 +0100

    collision

--- src/Core/Core.cpp
+++ src/Core/Core.cpp
@@ -20,6 +20,16 @@ ECS::Core::Core(GUI::IGUI &gui): _gui(gui)
     registry.add_component(e, component::Drawable("../assets/images/player_idle.png"));
     registry.add_component(e, component::Controllable(10, 0.9));
     registry.add_component(e, component::HitBox(100, 100));
+    registry.add_component(e, component::BoundsLimit(0, 0, 800, 600));
+
+    //create a entity at the right for test collision
+    entity e2 = registry.spawn_entity();
+    registry.add_component(e2, component::Position(500, 100));
+    registry.add_component(e2, component::Velocity(0, 0));
+    registry.add_component(e2, component::Drawable("../assets/images/player_idle.png"));
+    registry.add_component(e2, component::Controllable(10, 0.9));
+    registry.add_component(e2, component::HitBox(100, 100));
+    registry.add_component(e2, component::BoundsLimit(0, 0, 800, 600));
 }
 
 ECS::Core::~Core()
